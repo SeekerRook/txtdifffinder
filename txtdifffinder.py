37ac94f7448c3ba9da56945c9a133a5479599eac
SeekerRook <xrist.vardakis@gmail.com>
2021-07-27 01:42:31 +0300

    added support for multiple files to join

--- txtdifffinder.py
+++ txtdifffinder.py
@@ -2,6 +2,7 @@
 import sys
 filename2 = "out.txt"
 join = False
+joinl = []
 #read command line arguments
 if len(sys.argv) == 1:
     print("Error: no arguments given")
@@ -11,7 +12,7 @@ for i in range(2,len(sys.argv),2):
         filename2 = sys.argv[i+1]
 
     elif sys.argv[i] == "-j" :
-        filename3 = sys.argv[i+1]
+        joinl.append(sys.argv[i+1])
         join = True
     else:
         print("Error: uncnown argument ", sys.argv[i] )
@@ -27,9 +28,10 @@ file1 = open(filename1,"r")
 lines = file1.readlines()
 
 if join :
-    file3 = open(filename3,"r")
-    lines3 = file3.readlines()
-    lines += lines3    
+    for filename3 in joinl:
+        file3 = open(filename3,"r")
+        lines3 = file3.readlines()
+        lines += lines3    
 
 print ("Calculating.....")
 setofelems = {""}
